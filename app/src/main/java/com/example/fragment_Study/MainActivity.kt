1d551310289b6a13b5e6deb95cfd6101916446d2
kimjw2003 <kimjw111003@gmail.com>
2020-07-23 18:20:59 +0900

    [FINISH] Fragment Practice
    기본 프레그먼트 예제 연습

--- app/src/main/java/com/example/fragment_Study/MainActivity.kt
+++ app/src/main/java/com/example/fragment_Study/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.fragment_Study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setFrag(0)
+
+        Btn_fragment1.setOnClickListener{
+            setFrag(0)
+        }
+        Btn_fragment2.setOnClickListener {
+            setFrag(1)
+        }
+        Btn_fragment3.setOnClickListener {
+            setFrag(2)
+        }
+    }
+
+    private fun setFrag(fragNum : Int) {
+        val ft = supportFragmentManager.beginTransaction() //화면 교체를 위한 트랜잭션
+        when(fragNum){
+            0 -> {
+                ft.replace(R.id.main_frame, Fragment1()).commit()
+            }
+            1 ->{
+                ft.replace(R.id.main_frame, Fragment2()).commit()
+            }
+            2->{
+                ft.replace(R.id.main_frame, Fragment3()).commit()
+            }
+        }
     }
 }
