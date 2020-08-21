e636cee86ab97a9d265f9450286969c3f4061c28
kimjw2003 <kimjw111003@gmail.com>
2020-08-21 19:58:54 +0900

    [COMMIT]

--- app/src/main/java/com/example/fragment_Study/Fragment1.kt
+++ app/src/main/java/com/example/fragment_Study/Fragment1.kt
@@ -11,9 +11,6 @@ class Fragment1 : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment1, container, false)
 
-
-
-
         return view
     }
 }
